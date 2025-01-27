260708859073459cf7d02e39f093b75c4dc86d97
Syed Abdussami <syed.abdussami007@gmail.com>
2025-01-26 22:31:07 -0500

    configured docker-compose, and config server

---
+++ Services/Config-Server/src/test/java/com/E_Commerce_API/Config_Server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_Commerce_API.Config_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
