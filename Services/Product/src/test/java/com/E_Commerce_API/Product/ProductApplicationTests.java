79fd40e67c20c557418cd5c9c212c2b5d240d32f
Syed Abdussami <syed.abdussami007@gmail.com>
2025-01-31 17:22:33 -0500

    resolved port mapping for product service. connection established

---
+++ Services/Product/src/test/java/com/E_Commerce_API/Product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_Commerce_API.Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
